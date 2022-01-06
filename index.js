1903ad719a4de64abcbaf3d84268cb603c3c9a6e
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-07 04:30:00 +0800

    attempting to filter

--- index.js
+++ index.js
@@ -8,28 +8,39 @@ function handleClickEvent(event) {
         const res = await axios.get("https://api.data.gov.sg/v1/transport/carpark-availability");
         console.log("status", res.status);
 
+        // Print carpark number
+        const input = document.getElementById("carparkNumber");
+        //const inputString = JSON.stringify(input.value);
+
+        let numberOutput = document.getElementById("numberOutput")
+        numberOutput.innerText = input.value;
+
         // Convert timestamp into date and time outputs
         let timeStamp = res.data.items[0].timestamp;
         console.log('TimeStamp: ', timeStamp);
 
         let dateStamp = timeStamp.slice(0, 10);
         let dateOutput = document.getElementById('dateOutput');
-        dateOutput.innerText = 'dateStamp';
+        dateOutput.innerText = dateStamp;
         console.log('Date: ', dateStamp);
 
         let TimeStamp = timeStamp.slice(11, 16);
         let timeOutput = document.getElementById('timeOutput');
-        timeOutput.innerText = 'TimeStamp';
+        timeOutput.innerText = TimeStamp;
         console.log('Time: ', TimeStamp);
-
-        const input = document.getElementById("carparkNumber");
-        const inputString = JSON.stringify(input.value);
 
         // Extracting selected array
         let dataset = res.data.items[0].carpark_data;
         console.log(dataset);
-        let filteredCarpark = dataset
-        .filter(dataset => dataset.carpark_number === inputString)
+        let filteredCarpark = dataset.fetch();
+        filteredCarpark.filter(dataset, carpark_number) {
+            return new Promise((resolve) => {
+                resolve dataset.filter((dataset) => dataset[carpark_number] === input));
+            }
+        }
+        database.filter("carpark_number", input);
+
+
         .map(dataset => dataset.carpark_info);
         console.log(filteredCarpark);
     
