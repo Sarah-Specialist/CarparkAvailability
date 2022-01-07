72eb0d288a49d5ba4f51626458b428f1204d7f3b
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-07 17:58:40 +0800

    removed console.log

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const clickButtonEl = document.getElementById("submit");
 
 function handleClickEvent(event) {
-    console.log("button clicked");
     event.preventDefault();
 
     (async () => {
@@ -10,14 +9,12 @@ function handleClickEvent(event) {
 
         // Print carpark number
         const input = document.getElementById("carparkNumber");
-        //const inputString = JSON.stringify(input.value);
 
         let numberOutput = document.getElementById("numberOutput")
         numberOutput.innerText = input.value;
 
         // Convert timestamp into date and time outputs
         let timeStamp = res.data.items[0].timestamp;
-        console.log('TimeStamp: ', timeStamp);
 
         let dateStamp = timeStamp.slice(0, 10);
         let dateOutput = document.getElementById('dateOutput');
@@ -31,40 +28,32 @@ function handleClickEvent(event) {
 
         // Extracting selected array
         let dataset = res.data.items[0].carpark_data;
-        console.log(dataset);
         let filteredCarpark = dataset.filter(function(dataset) {
-           if (dataset.carpark_number === "HLM") {
+           if (dataset.carpark_number === input.value) {
                return dataset;
            };
         })
-        console.log(filteredCarpark);
-        //let carparkInfo = Object.keys(filteredCarpark);
 
         let carparkInfo = filteredCarpark.map(e => {
             return e.carpark_info;
         })
-        console.log(carparkInfo);
-
-        let infoString = JSON.stringify(carparkInfo);
-        console.log('infoString', infoString);
     
         //Extracting information from selected array and inserting extracted value
+        let infoString = JSON.stringify(carparkInfo);
+        
         let lotsOutput = document.getElementById('lotsOutput');
-        let totalLots = infoString.slice(17, 20);
-        //filteredCarpark[0][0].total_lots;
-        lotsOutput.innerText(totalLots);
+        let totalLots = (infoString.slice(15, 22)).replace(/[^0-9]/g, '');
+        lotsOutput.innerText = totalLots;
         console.log('Total lots: ', totalLots);
 
         let typeOutput = document.getElementById('typeOutput');
-        let lotType = infoString.slice(34, 35);
-        //filteredCarpark[0][0].lot_type;
-        typeOutput.innerText(lotType);
+        let lotType = (infoString.slice(32, 36)).replace(/[^a-zA-Z ]/g, "");
+        typeOutput.innerText = lotType;
         console.log('Lot type: ', lotType);
 
         let availableOutput = document.getElementById('availableOutput');
-        let lotsAvailable = infoString.slice(54,58);
-        //filteredCarpark[0][0].lots_available;
-        availableOutput.innerText(lotsAvailable);
+        let lotsAvailable = (infoString.slice(50,60)).replace(/[^0-9]/g, '');
+        availableOutput.innerText = lotsAvailable;
         console.log('Lots available: ', lotsAvailable);
     })();
 };
